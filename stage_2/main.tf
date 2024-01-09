24985a470df4e9f8ed7fdfeca6f6c46561715bd6
kryptoblack <thisguy@pratikthakare.com>
2024-01-09 23:22:50 +0530

    fix: fixed site hosting import

--- stage_2/main.tf
+++ stage_2/main.tf
@@ -6,28 +6,28 @@ data "terraform_remote_state" "stage_1" {
   }
 }
 
-# Firebase Web App
-/*
- * resource "google_firebase_web_app" "default" {
- *   provider        = google-beta
- *   project         = data.terraform_remote_state.stage_1.outputs.google_firebase_project.project
- *   display_name    = "Admin Panel"
- *   deletion_policy = "DELETE"
- * }
- */
 
-# Firebase Hosting
+# Firebase Hosting and Web App
+resource "google_firebase_web_app" "default" {
+  provider        = google-beta
+  project         = data.terraform_remote_state.stage_1.outputs.google_firebase_project.project
+  display_name    = "Admin Panel"
+  deletion_policy = "DELETE"
+}
+
 import {
   provider = google-beta
 
-  # Variable is used instead of remote state output because import requires values
-  # to resolve during plan phase.
-  id = var.firebase_project_id
+  # Variable is used instead of remote state output because import requires 
+  # values to resolve during plan phase.
+  id = "projects/${var.firebase_project_id}/sites/${var.firebase_project_id}"
   to = google_firebase_hosting_site.default
 }
 
-# resource "google_firebase_hosting_site" "default" {
-# provider = google-beta
-# project  = data.terraform_remote_state.stage_1.outputs.google_firebase_project.project
-# # app_id   = google_firebase_web_app.default.app_id
-# }
+resource "google_firebase_hosting_site" "default" {
+  provider = google-beta
+  project  = data.terraform_remote_state.stage_1.outputs.google_firebase_project.project
+  site_id  = data.terraform_remote_state.stage_1.outputs.google_firebase_project.project
+  app_id   = google_firebase_web_app.default.app_id
+}
+
